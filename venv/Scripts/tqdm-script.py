6176ac49a196d3786a4a50cc50c0306b99bb080f
enkelian <aniano13@gmail.com>
2020-05-27 14:10:12 +0200

    fix

---
+++ venv/Scripts/tqdm-script.py
@@ -0,0 +1,12 @@
+#!E:\Enkeli\studia\mownit_lab\venv\Scripts\python.exe
+# EASY-INSTALL-ENTRY-SCRIPT: 'tqdm==4.45.0','console_scripts','tqdm'
+__requires__ = 'tqdm==4.45.0'
+import re
+import sys
+from pkg_resources import load_entry_point
+
+if __name__ == '__main__':
+    sys.argv[0] = re.sub(r'(-script\.pyw?|\.exe)?$', '', sys.argv[0])
+    sys.exit(
+        load_entry_point('tqdm==4.45.0', 'console_scripts', 'tqdm')()
+    )
